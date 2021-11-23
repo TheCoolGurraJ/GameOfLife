9c9eb4b6d4f4c85e49148bc30e8b2f1de42383a5
Gustav Jonsson <50779961+TheCoolGurraJ@users.noreply.github.com>
2021-11-23 10:13:07 +0100

    added code

---
+++ Game of Life/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace Game_of_Life
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Board = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Board
+            // 
+            this.Board.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Board.AutoSize = true;
+            this.Board.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Board.Location = new System.Drawing.Point(12, 82);
+            this.Board.Margin = new System.Windows.Forms.Padding(0);
+            this.Board.Name = "Board";
+            this.Board.Size = new System.Drawing.Size(1100, 550);
+            this.Board.TabIndex = 2;
+            this.Board.Paint += new System.Windows.Forms.PaintEventHandler(this.Board_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1174, 673);
+            this.Controls.Add(this.Board);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel Board;
+    }
+}
+
---
+++ Game of Life/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_of_Life
+{
+    public partial class Form1 : Form
+    {
+        public class Cell
+        {
+            public bool IsAlive { get; set; }
+            public int X; //X-coordinate
+            public int Y; //Y-coordinate
+
+            public List<Cell> neighbours = new List<Cell>();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public bool runSim = false;
+        public List<Cell> cells = new List<Cell>();
+        public int nmrOfrows = 10;
+        public int nmrOfcols = 10;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(!runSim)
+            {
+                runSim = true;
+            }
+            else
+            {
+                runSim = false;
+            }
+            Simulation(runSim);
+        }
+
+        void Simulation(bool run)
+        {
+
+            if(run) //Game
+            {
+                Cell item = new Cell();
+                item.X = 2;
+                item.Y = 2;
+                cells.Add(item);
+
+                Cell test = new Cell();
+                test.X = 8;
+                test.Y = 1;
+                cells.Add(test);
+
+                Render();
+            }
+        }
+
+        void Render()
+        {
+            int[,] GridArr = new int[nmrOfrows, nmrOfcols]; // Want to store cells in 2D array...
+            Pen blue = new Pen(Color.Blue);
+
+            foreach (Cell c in cells)
+            {
+                int coldist = ((Board.Width / 2) / nmrOfcols); //Calculate distance for cell in grid
+                int rowdist = (Board.Height / nmrOfrows);
+
+                PictureBox pc = new PictureBox();
+
+                pc.Margin = new Padding(0);
+                pc.Padding = new Padding(0); // not working??
+
+                pc.Location = new Point (coldist*c.X, rowdist*c.Y); //Spawnpoint of cell
+                pc.BackColor = Color.Blue;
+                pc.Parent = Board;
+                pc.Size = new Size(coldist, rowdist);
+            }           
+        }
+
+        private void Board_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen p = new Pen(Color.Black);
+
+            for(int i = 1;i<nmrOfrows;i++)
+            {
+                g.DrawLine(p,0,(Board.Height/nmrOfrows)*i,Board.Width,(Board.Height/nmrOfrows)*i);
+            }
+
+            for (int i = 1; i < nmrOfcols*2; i++)
+            {
+                g.DrawLine(p, ((Board.Width/2) / nmrOfcols) * i, 0,((Board.Width/2) / nmrOfcols) * i, Board.Height);
+            }
+        }
+    }
+}
