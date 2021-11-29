6446c6bfc3943a28da2bbb4cb51ca1e2a0ecef45
Gustav Jonsson <50779961+TheCoolGurraJ@users.noreply.github.com>
2021-11-29 12:02:38 +0100

    can add cells now

--- Game of Life/Form1.Designer.cs
+++ Game of Life/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.Board = new System.Windows.Forms.Panel();
+            this.Addbtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -47,22 +48,35 @@
             this.Board.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.Board.AutoSize = true;
             this.Board.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.Board.Location = new System.Drawing.Point(12, 82);
+            this.Board.Location = new System.Drawing.Point(12, 75);
             this.Board.Margin = new System.Windows.Forms.Padding(0);
+            this.Board.MinimumSize = new System.Drawing.Size(1100, 550);
             this.Board.Name = "Board";
             this.Board.Size = new System.Drawing.Size(1100, 550);
             this.Board.TabIndex = 2;
             this.Board.Paint += new System.Windows.Forms.PaintEventHandler(this.Board_Paint);
             // 
+            // Addbtn
+            // 
+            this.Addbtn.Location = new System.Drawing.Point(111, 12);
+            this.Addbtn.Name = "Addbtn";
+            this.Addbtn.Size = new System.Drawing.Size(93, 38);
+            this.Addbtn.TabIndex = 3;
+            this.Addbtn.Text = "Add";
+            this.Addbtn.UseVisualStyleBackColor = true;
+            this.Addbtn.Click += new System.EventHandler(this.Addbtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1174, 673);
+            this.ClientSize = new System.Drawing.Size(1436, 673);
+            this.Controls.Add(this.Addbtn);
             this.Controls.Add(this.Board);
             this.Controls.Add(this.button1);
+            this.MinimumSize = new System.Drawing.Size(1458, 729);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "GameOfLife";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,6 +86,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Panel Board;
+        private System.Windows.Forms.Button Addbtn;
     }
 }
 
--- Game of Life/Form1.cs
+++ Game of Life/Form1.cs
@@ -17,6 +17,7 @@ namespace Game_of_Life
             public bool IsAlive { get; set; }
             public int X; //X-coordinate
             public int Y; //Y-coordinate
+            public PictureBox cellbox;
 
             public List<Cell> neighbours = new List<Cell>();
         }
@@ -24,12 +25,38 @@ namespace Game_of_Life
         public Form1()
         {
             InitializeComponent();
+
+
+            int coldist = ((Board.Width / 2) / nmrOfcols); //Calculate distance for cell in grid
+            int rowdist = (Board.Height / nmrOfrows);
+
+
+            for (int i = 0; i < nmrOfrows; i++)
+            {
+                GridArr[i] = new int[nmrOfcols * 2];
+                for (int k = 0; k < nmrOfcols*2; k++)
+                {
+                    PictureBox pc = new PictureBox();
+                    pc.Location = new Point(coldist * k, rowdist * i); //Spawnpoint of pictureboxes
+                    pc.Parent = Board;
+                    pc.Size = new Size(coldist, rowdist);
+                    pc.Parent = Board;
+                    pc.MouseClick += new System.Windows.Forms.MouseEventHandler(pc_MouseClick);
+                    pclist.Add(pc);
+                    GridArr[i][k] = pclist.IndexOf(pc);
+                    pc.BackColor = Color.Transparent;
+                }
+            }
         }
+
         public bool runSim = false;
         public List<Cell> cells = new List<Cell>();
-        public int nmrOfrows = 10;
-        public int nmrOfcols = 10;
-
+        public static int nmrOfrows = 10;
+        public static int nmrOfcols = 10; 
+        public bool hasClicked = false;
+        public List<PictureBox> pclist = new List<PictureBox>();
+        public int[][] GridArr = new int[nmrOfrows][]; 
+        
         private void button1_Click(object sender, EventArgs e)
         {
             if(!runSim)
@@ -48,40 +75,15 @@ namespace Game_of_Life
 
             if(run) //Game
             {
-                Cell item = new Cell();
-                item.X = 2;
-                item.Y = 2;
-                cells.Add(item);
+                foreach(Cell c in cells)
+                {
+                    if(c.X>0 && c.X < nmrOfcols*2)
+                    {
 
-                Cell test = new Cell();
-                test.X = 8;
-                test.Y = 1;
-                cells.Add(test);
+                    }
 
-                Render();
+                }
             }
-        }
-
-        void Render()
-        {
-            int[,] GridArr = new int[nmrOfrows, nmrOfcols]; // Want to store cells in 2D array...
-            Pen blue = new Pen(Color.Blue);
-
-            foreach (Cell c in cells)
-            {
-                int coldist = ((Board.Width / 2) / nmrOfcols); //Calculate distance for cell in grid
-                int rowdist = (Board.Height / nmrOfrows);
-
-                PictureBox pc = new PictureBox();
-
-                pc.Margin = new Padding(0);
-                pc.Padding = new Padding(0); // not working??
-
-                pc.Location = new Point (coldist*c.X, rowdist*c.Y); //Spawnpoint of cell
-                pc.BackColor = Color.Blue;
-                pc.Parent = Board;
-                pc.Size = new Size(coldist, rowdist);
-            }           
         }
 
         private void Board_Paint(object sender, PaintEventArgs e)
@@ -98,6 +100,37 @@ namespace Game_of_Life
             {
                 g.DrawLine(p, ((Board.Width/2) / nmrOfcols) * i, 0,((Board.Width/2) / nmrOfcols) * i, Board.Height);
             }
+        }
+
+        private void Addbtn_Click(object sender, EventArgs e)
+        {
+            hasClicked = false;
+        }
+
+        private void pc_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            PictureBox pc = (PictureBox)sender;
+
+            hasClicked = true;
+            Cell item = new Cell();
+            item.cellbox = pc;
+
+            foreach(int[] row in GridArr)
+            {
+                for (int i = 0; i < row.Length; i++)
+                {
+                    if (row[i] == pclist.IndexOf(pc))
+                    {
+                        item.Y = Array.IndexOf(GridArr, row);
+                        item.X = i;
+                    }
+                }
+            }
+            item.cellbox.BackColor = Color.Blue;
+            item.IsAlive = true;
+            cells.Add(item);
+
         }
     }
 }
